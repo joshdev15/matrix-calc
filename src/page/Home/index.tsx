216d3a2ff9c5baed080e5636f7addf39e799a5ac
Joshua <joshuaalexandermora@gmail.com>
2024-05-11 19:32:17 -0400

    Terminated: Sum

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -4,10 +4,47 @@ import styles from "./styles.module.scss";
 
 const Home = () => {
   const [base, setBase] = useState(2);
+  const [finalResult, setResult] = useState<never[][]>();
 
   const getFormData = () => {
+    // Obtener elementos visuales
+    setResult(undefined);
     const inputs = document.querySelectorAll("input");
-    console.log("inputs", inputs);
+
+    // Limpieza del Arreglo A
+    const orderedArrayA = Array.from({ length: base }).map((_) => []);
+    const arrayA = Array.from(inputs).filter((element) =>
+      element.id.includes("a-"),
+    );
+
+    arrayA.forEach((item) => {
+      const id = item.id.split("-");
+      const arrLevel = parseInt(id[1]);
+      orderedArrayA[arrLevel].push(parseInt(item.value) as never);
+    });
+
+    // Limpieza del Arreglo B
+    const orderedArrayB = Array.from({ length: base }).map((_) => []);
+    const arrayB = Array.from(inputs).filter((element) =>
+      element.id.includes("b-"),
+    );
+
+    arrayB.forEach((item) => {
+      const id = item.id.split("-");
+      const arrLevel = parseInt(id[1]);
+      orderedArrayB[arrLevel].push(parseInt(item.value) as never);
+    });
+
+    // Calculando resultado
+    const resultArray = Array.from({ length: base }).map((_) => []);
+
+    orderedArrayA.forEach((i: any, aidx: number) => {
+      i.forEach((num: any, bidx: number) => {
+        resultArray[aidx][bidx] = (num + orderedArrayB[aidx][bidx]) as never;
+      });
+    });
+
+    setResult(resultArray);
   };
 
   return (
@@ -29,8 +66,8 @@ const Home = () => {
               <tr key={`ArrA${indexOne}`}>
                 {Array.from({ length: base }).map(
                   (_: any, indexTwo: number) => (
-                    <td key={`ArrA${indexOne}-Item${indexTwo}`}>
-                      <AppInput id={`ArrA${indexOne}-Item${indexTwo}`} />
+                    <td key={`a-${indexOne}-${indexTwo}`}>
+                      <AppInput id={`a-${indexOne}-${indexTwo}`} />
                     </td>
                   ),
                 )}
@@ -47,8 +84,8 @@ const Home = () => {
               <tr key={`ArrB${indexOne}`}>
                 {Array.from({ length: base }).map(
                   (_: any, indexTwo: number) => (
-                    <td key={`ArrB${indexOne}-Item${indexTwo}`}>
-                      <AppInput id={`ArrB${indexOne}-Item${indexTwo}`} />
+                    <td key={`b-${indexOne}-${indexTwo}`}>
+                      <AppInput id={`b-${indexOne}-${indexTwo}`} />
                     </td>
                   ),
                 )}
@@ -63,6 +100,25 @@ const Home = () => {
           Sumar
         </button>
       </div>
+
+      {finalResult !== undefined && (
+        <table style={{ marginTop: 20 }}>
+          <tbody>
+            {finalResult.map((level, index) => (
+              <tr key={`arr${index}`}>
+                {level.map((value: any, indexValue: number) => (
+                  <td
+                    className={styles.squareInput}
+                    key={`result-${index}-${indexValue}`}
+                  >
+                    {value}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
